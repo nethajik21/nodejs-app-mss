c230be676fa9d37cefeb0572f09a7e7f042640b7
Nethaji K <106616801+nethajik21@users.noreply.github.com>
2023-12-20 10:25:25 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://3.110.118.32:9000/',
+  serverUrl: 'http://43.204.111.118:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
